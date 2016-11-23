46ab42fc0c29b0f5446caec3dcd138e3e2ae6f0a
Galih Ristra <galih.ristra@gmail.com>
2016-11-23 21:19:23 +0700

    inisiasi model

---
+++ AtriumXsisV4.WebApps/AtriumXsisV4.WebApps/Models/EmployeeModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtriumXsisV4.WebApps.Models
+{
+    public class EmployeeModels
+    {
+        public EmployeeModels()
+        {
+
+        }
+
+        public string EMPLOYEEID { get; set; }
+        public string NAME { get; set; }
+        public string ADDRESS { get; set; }
+        public string EMAIL { get; set; }
+        public string PHONENUMBER { get; set; }
+        public int SALARY { get; set; }
+        public string POSITIONID { get; set; }
+        public string BOOTCAMP { get; set; }
+        public string PROJECTID { get; set; }
+        public DateTime? DCREA { get; set; }
+        public string VCREA { get; set; }
+        public DateTime? DMODI { get; set; }
+        public string VMODI { get; set; }
+    }
+}
---
+++ AtriumXsisV4.WebApps/AtriumXsisV4.WebApps/Models/TimesheetModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtriumXsisV4.WebApps.Models
+{
+    public class TimesheetModels
+    {
+        public TimesheetModels()
+        {
+
+        }
+
+        public Guid ID { get; set; }
+        public string EMPLOYEEID { get; set; }
+        public DateTime DWORKDATE { get; set; }
+        public string VSTATUS { get; set; }
+        public DateTime? DSTARTWORK { get; set; }
+        public DateTime? DENDWORK { get; set; }
+        public DateTime? DSTARTOT { get; set; }
+        public DateTime? DENDOT { get; set; }
+        public string DESCRIPTION { get; set; }
+        public DateTime? DCREA { get; set; }
+        public string VCREA { get; set; }
+        public DateTime? DMODI { get; set; }
+        public string VMODI { get; set; }
+    }
+}
